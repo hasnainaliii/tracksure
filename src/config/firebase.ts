6a51e2af59a7484a7c2f405b45f5cceda77ea9ac
Hasnain Ali <eyemusican333@gmail.com>
2025-07-18 19:21:04 +0500

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -9,12 +9,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCi-RSr8b8JllN2tfTUoaNnSerC1dp1sEA",
-  authDomain: "tracksure-641c3.firebaseapp.com",
-  projectId: "tracksure-641c3",
-  storageBucket: "tracksure-641c3.firebasestorage.app",
-  messagingSenderId: "657558324457",
-  appId: "1:657558324457:web:e30786c92d93b63ba51c78",
+  
 };
 
 // Initialize Firebase
